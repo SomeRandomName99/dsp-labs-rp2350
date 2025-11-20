4ed8d96b026bb0ae5575fbc91bb52f342f10acd2
SomeRandomName <husam.malkawi@hotmail.com>
2025-11-20 22:48:08 +0100

    Make ring buffer implementation use its full capacity

--- lcavDSPLabs/audioPassThrough/firmware_core/ring_buffer.h
+++ lcavDSPLabs/audioPassThrough/firmware_core/ring_buffer.h
@@ -3,10 +3,11 @@
 
 #include "pico/stdlib.h"
 
-// A non thread safe implementation of a ring buffer
+/* A single-producer single-consumer ring buffer implementation
+*/
 typedef struct {
-  volatile uint16_t head;
-  volatile uint16_t tail;
+  volatile uint16_t write_index;
+  volatile uint16_t read_index;
   const uint16_t capacity;
   const uint16_t element_size;
   uint8_t* const buffer; 
@@ -16,11 +17,18 @@ typedef struct {
 
 // Internal implemetation macro that is used to avoid code duplication.
 // Use the definition below
+  /* A power of 2 number of elements is a must for fast circular increment and 
+  being able to use all of the capacity */ 
+  /* In order to be able to differentiate between full and empty, the last bit
+  in the index will be used for that purpose. Hence the maximum allowed capacity
+  is one less than the maximum power of two of the index variable */ 
 #define _rb_init(linkage, name, num_elements, size_of_element) \
-  linkage uint8_t _rb_buffer_##name[num_elements*size_of_element]; \
+  static_assert((num_elements > 0) && ((num_elements & (num_elements - 1)) == 0)); \
+  static_assert(num_elements <= (1 << 15));\
+  linkage uint8_t _rb_buffer_##name[num_elements*size_of_element] __attribute__((aligned(4))); \
   linkage ring_buffer_t name = { \
-    .head = 0, \
-    .tail = 0, \
+    .write_index = 0, \
+    .read_index = 0, \
     .capacity = num_elements, \
     .element_size = size_of_element, \
     .buffer = _rb_buffer_##name \
@@ -33,60 +41,54 @@ typedef struct {
   _rb_init(/* no linkage */, name, num_elements, size_of_element)
 
 /* Public Functions */
-static inline uint16_t circular_increment(ring_buffer_t *rb, uint16_t index){
-  if(++index == rb->capacity) index = 0;
-  return index;
+static inline uint16_t size(ring_buffer_t *rb) {
+  return rb-> write_index - rb-> read_index;
 }
 
 static inline bool rb_is_full(ring_buffer_t *rb) {
-  return circular_increment(rb, rb->head) == rb->tail;
+  return (rb->write_index - rb->read_index) == rb->capacity;
 }
 
 static inline bool rb_is_empty(ring_buffer_t *rb) {
-  return rb->head == rb->tail;
+  return rb->write_index == rb->read_index;
 }
 
 static inline uint8_t* rb_get_write_buffer(ring_buffer_t *rb) {
   assert(!rb_is_full(rb));
-  return &rb->buffer[rb->head * rb->element_size];
+  uint8_t write_index = rb->write_index & (rb->capacity - 1);
+  return &rb->buffer[write_index * rb->element_size];
 }
 
 static inline void rb_increment_write_index(ring_buffer_t *rb) {
   assert(!rb_is_full(rb));
-  rb->head = circular_increment(rb, rb->head);
+  rb->write_index++;
 }
 
 static inline uint8_t* rb_get_read_buffer(ring_buffer_t *rb) {
   assert(!rb_is_empty(rb));
-  return &rb->buffer[rb->tail * rb->element_size];
+  uint8_t read_index = rb->read_index & (rb->capacity - 1);
+  return &rb->buffer[read_index * rb->element_size];
 }
 
 static inline void rb_increment_read_index(ring_buffer_t *rb) {
   assert(!rb_is_empty(rb));
-  rb->tail = circular_increment(rb, rb->tail);
+  rb->read_index++;
 }
 
 static inline void rb_increase_read_index(ring_buffer_t *rb, uint16_t count) {
-  uint16_t new_index = rb->tail + count;
-  if(new_index >= rb->capacity) {
-    new_index -= rb->capacity;
-  }
-  assert(new_index <= rb->head);
-  rb->tail = new_index;
+  assert(size(rb) >= count);
+  rb->read_index += count;
 }
 
 static inline uint8_t* rb_read_at_index(ring_buffer_t *rb, uint16_t index) {
-  assert(index < rb->capacity);
-  uint16_t read_index = rb->tail + index;
-  if(read_index >= rb->capacity) {
-    read_index -= rb->capacity;
-  }
+  assert(index < size(rb));
+  uint16_t read_index = (rb->read_index + index) & (rb->capacity - 1);
   return &rb->buffer[read_index * rb->element_size];
 }
 
 static inline void rb_reset(ring_buffer_t *rb) {
-  rb->head = 0;
-  rb->tail = 0;
+  rb->write_index = 0;
+  rb->read_index = 0;
 }
 
 #endif // RING_BUFFER_H
