d7cebf548042961018ea807050d6e3a042d1e906
SomeRandomName <husam.malkawi@hotmail.com>
2025-11-20 22:48:43 +0100

    Update core to work with the new ring_buffer

--- lcavDSPLabs/audioPassThrough/firmware_core/audio_bus.c
+++ lcavDSPLabs/audioPassThrough/firmware_core/audio_bus.c
@@ -2,8 +2,8 @@
 #include "ring_buffer.h"
 #include "audio_bus.h"
 
-rb_init_shared(g_i2s_to_proc_buffer, 3, AUDIO_PACKET_SIZE);
-rb_init_shared(g_proc_to_usb_buffer, 3, AUDIO_PACKET_SIZE);
+rb_init_shared(g_i2s_to_proc_buffer, 4, AUDIO_PACKET_SIZE);
+rb_init_shared(g_proc_to_usb_buffer, 4, AUDIO_PACKET_SIZE);
 bool audio_mute = false;
 float audio_volume_multiplier = 1.0f;
 
